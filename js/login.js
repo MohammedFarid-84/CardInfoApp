f5229c57a6e9d0b8ebe715155aaba513b478713a
El-Hlawany <aboabdallahhlawany@gmail.com>
2024-11-22 17:40:22 +0200

    edit library path

--- js/login.js
+++ js/login.js
@@ -1,5 +1,5 @@
 // تحميل مكتبة SQL.js
-const config = { locateFile: filename => "../libs/sql-wasm.wasm" };
+const config = { locateFile: filename => '../libs/${filename}' };
 
 initSqlJs(config).then(SQL => {
     // تحميل قاعدة البيانات
